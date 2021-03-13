d5bf62c37dd315fcefdca52fb9e302f99c759604
bachra <bachbrahem@gmail.cm>
2021-03-13 09:51:01 +0100

    removed cname

--- tp_web1/js/script.js
+++ tp_web1/js/script.js
@@ -1,14 +1,7 @@
-console.log("boo");
-
-// function invertHex(hex) {
-    // return (Number(`0x1${hex}`) ^ 0xFFFFFF).toString(16).substr(1).toUpperCase()
-// }
+// function to invert colors so text color can be visible 
 function invertHex(hex) {
     if (hex.indexOf('#') === 0) {
         hex = hex.slice(1);
-    }
-    if (hex.length !== 6) {
-        throw new Error('Invalid HEX color.');
     }
     var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
         g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
@@ -21,8 +14,11 @@ function padZero(str, len) {
     var zeros = new Array(len).join('0');
     return (zeros + str).slice(-len);
 }
+
+
 function highlight(name) {
     var a = document.getElementById(name);
+    //generates a random hex
     var randomBgColor ="#"+ Math.floor(Math.random() * 16777215).toString(16);
     var inverseColor=invertHex(randomBgColor);
 
@@ -31,17 +27,11 @@ function highlight(name) {
     a.style.backgroundColor = randomBgColor;
     a.style.color=inverseColor;
 }
-
+// add event handlers
 for (var i = 0; i < 5; i++) {
-
-    document.getElementById("l" + i).addEventListener("click", myFunction);
-    document.getElementById("l" + i).addEventListener("click", myFunction);
-    document.getElementById("l" + i).addEventListener("click", myFunction);
-    document.getElementById("l" + i).addEventListener("click", myFunction);
     document.getElementById("l" + i).addEventListener("click", myFunction);
 }
 
 function myFunction(e) {
-    console.log("hehe");
     highlight(e.target.id);
 }
