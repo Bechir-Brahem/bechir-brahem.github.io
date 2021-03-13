62e1c9384956186c5ca0bf9ec344a4b95f6b1a06
bachra <bachbrahem@gmail.cm>
2021-03-13 09:28:07 +0100

    added inverted colors

--- tp_web1/js/script.js
+++ tp_web1/js/script.js
@@ -1,10 +1,35 @@
 console.log("boo");
 
+// function invertHex(hex) {
+    // return (Number(`0x1${hex}`) ^ 0xFFFFFF).toString(16).substr(1).toUpperCase()
+// }
+function invertHex(hex) {
+    if (hex.indexOf('#') === 0) {
+        hex = hex.slice(1);
+    }
+    if (hex.length !== 6) {
+        throw new Error('Invalid HEX color.');
+    }
+    var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+        g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+        b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+    return '#' + padZero(r) + padZero(g) + padZero(b);
+}
+
+function padZero(str, len) {
+    len = len || 2;
+    var zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
+}
 function highlight(name) {
     var a = document.getElementById(name);
-    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    var randomBgColor ="#"+ Math.floor(Math.random() * 16777215).toString(16);
+    var inverseColor=invertHex(randomBgColor);
 
-    a.style.backgroundColor = "#" + randomColor;
+    console.log(randomBgColor,inverseColor)
+
+    a.style.backgroundColor = randomBgColor;
+    a.style.color=inverseColor;
 }
 
 for (var i = 0; i < 5; i++) {
@@ -20,4 +45,3 @@ function myFunction(e) {
     console.log("hehe");
     highlight(e.target.id);
 }
-
